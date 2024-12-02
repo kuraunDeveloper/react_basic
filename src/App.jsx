088c429030e566ac7e1ef0798c9aded1cc72b521
kuraunDeveloper <kuraunDeveloper@gmail.com>
2024-12-02 11:21:49 +0900

    ColorfulMessageを追加、Reactのスタイルの当て方を学ぶ

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { ColorfulMessage } from "./components/ColorfulMessage";
+
 export const App = () => {
+    const onClickButton = () => alert();
     return (
         <div>
-            <h1>こんにちは</h1>
-            <p>hoge</p>
+            <h1 style={{ color: "#aacf00" }}>こんにちは</h1>
+            <ColorfulMessage color="blue">みやんご</ColorfulMessage>
+            <ColorfulMessage color="green">やんぼ</ColorfulMessage>
+            <button onClick={onClickButton}>ボタン</button>
         </div>
     );
 };
