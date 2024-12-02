9c73d26ddca03a7ada4c6f7a014a555114648930
kuraunDeveloper <kuraunDeveloper@gmail.com>
2024-12-02 12:11:15 +0900

    useEffectを知る

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
+    console.log("--App--");
     const [num, setNum] = useState(0);
+    const [isShowMiya, setIsShowMiya] = useState(false);
     const onClickCountUp = () => {
         setNum((prev) => prev + 1);
-        setNum((prev) => prev + 1);
     };
+    const onClickToggle = () => {
+        setIsShowMiya(!isShowMiya);
+    };
+
+    useEffect(() => {
+        if (num > 0 && num % 3 === 0) {
+            isShowMiya || setIsShowMiya(true);
+        } else {
+            isShowMiya && setIsShowMiya(false);
+        }
+    }, [num]);
+
     return (
-        <div>S
-            <h1 style={{ color: "#aacf00" }}>こんにちは</h1>
+        <div>
+            <h1 style={{ color: "#aacfff" }}>こんにちは</h1>
             <ColorfulMessage color="blue">みやんご</ColorfulMessage>
             <ColorfulMessage color="green">やんぼ</ColorfulMessage>
             <button onClick={onClickCountUp}>カウントアップ</button>
             <p>{num}</p>
+            <button onClick={onClickToggle}>on/off</button>
+            {isShowMiya && 
+            <p>みやたろう</p>}
         </div>
     );
 };
