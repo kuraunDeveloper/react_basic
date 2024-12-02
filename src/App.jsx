e08e233e1f6931d1995ff77556fca87dbef572d9
kuraunDeveloper <kuraunDeveloper@gmail.com>
2024-12-02 11:34:51 +0900

    stateを学ぶ

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
-    const onClickButton = () => alert();
+    const [num, setNum] = useState(0);
+    const onClickCountUp = () => {
+        setNum((prev) => prev + 1);
+        setNum((prev) => prev + 1);
+    };
     return (
-        <div>
+        <div>S
             <h1 style={{ color: "#aacf00" }}>こんにちは</h1>
             <ColorfulMessage color="blue">みやんご</ColorfulMessage>
             <ColorfulMessage color="green">やんぼ</ColorfulMessage>
-            <button onClick={onClickButton}>ボタン</button>
+            <button onClick={onClickCountUp}>カウントアップ</button>
+            <p>{num}</p>
         </div>
     );
 };
